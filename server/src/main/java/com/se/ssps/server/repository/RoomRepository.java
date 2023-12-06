ed07e016d461661df3f6a1a03272f2f4f13b021c
Tuan <tuan.kieudangquoc03@hcmut.edu.vn>
2023-12-07 03:45:36 +0700

    fix update room

--- server/src/main/java/com/se/ssps/server/repository/RoomRepository.java
+++ server/src/main/java/com/se/ssps/server/repository/RoomRepository.java
@@ -1,16 +1,21 @@
 package com.se.ssps.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.se.ssps.server.entity.configuration.Room;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
     @Query("SELECT r FROM Room r where r.id = ?1")
     public Room findRoomById(Integer id);
 
+    @Modifying
+    @Transactional
     @Query("update Room r set r.havePrinter = true where r.id = ?1")
     public void roomHavePrinter(Integer roomId);
 }
