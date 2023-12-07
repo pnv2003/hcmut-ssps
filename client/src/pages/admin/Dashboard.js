05c64fb47670553f3999c0a887692fc3800a5e64
Phuong <phuongngovan2003@gmail.com>
2023-12-07 22:30:41 +0700

    Reset stat

--- client/src/pages/admin/Dashboard.js
+++ client/src/pages/admin/Dashboard.js
@@ -17,58 +17,58 @@ export default function Dashboard() {
     // profit by month (PBM)
     const [pbmStat, setPbmStat] = useState([]);
 
-    useEffect(() => {
-        // get PBP
-        sendGetRequest(
-            '/admin/statistics/pages-by-printer?from=' + start + '&to=' + end,
-            'cannot get PBP statistics'
-        ).then((data) => {
-            const initStat = data.map((elem) => {
-                return {
+    // useEffect(() => {
+    //     // get PBP
+    //     sendGetRequest(
+    //         '/admin/statistics/pages-by-printer?from=' + start + '&to=' + end,
+    //         'cannot get PBP statistics'
+    //     ).then((data) => {
+    //         const initStat = data.map((elem) => {
+    //             return {
 
 
-                }   
-            });
-        });
+    //             }   
+    //         });
+    //     });
 
-        // get POPR
-        sendGetRequest(
-            '/admin/statistics/request-by-printer?from=' + start + '&to=' + end,
-            'cannot get POPR statistics'
-        ).then((data) => {
-            const initStat = data.map((elem) => {
-                return {
+    //     // get POPR
+    //     sendGetRequest(
+    //         '/admin/statistics/request-by-printer?from=' + start + '&to=' + end,
+    //         'cannot get POPR statistics'
+    //     ).then((data) => {
+    //         const initStat = data.map((elem) => {
+    //             return {
 
-                }
-            })
-        })
+    //             }
+    //         })
+    //     })
 
-        // get POPS
-        sendGetRequest(
-            '/admin/statistics/size-by-month?from=' + start + '&to=' + end,
-            'cannot get POPS statistics'
-        ).then((data) => {
-            const initStat = data.map((elem) => {
-                return {
+    //     // get POPS
+    //     sendGetRequest(
+    //         '/admin/statistics/size-by-month?from=' + start + '&to=' + end,
+    //         'cannot get POPS statistics'
+    //     ).then((data) => {
+    //         const initStat = data.map((elem) => {
+    //             return {
                     
-                }
-            });
-        })
+    //             }
+    //         });
+    //     })
 
-        // get PBM
-        sendGetRequest(
-            '/admin/statistics/profit-by-month?from=' + start + '&to=' + end,
-            'cannot get PBM statistics'
-        ).then((data) => {
-            const initStat = data.map((elem) => {
-                return {
+    //     // get PBM
+    //     sendGetRequest(
+    //         '/admin/statistics/profit-by-month?from=' + start + '&to=' + end,
+    //         'cannot get PBM statistics'
+    //     ).then((data) => {
+    //         const initStat = data.map((elem) => {
+    //             return {
 
-                }
-            });
-        })
+    //             }
+    //         });
+    //     })
 
         
-    }, [start, end]);
+    // }, [start, end]);
 
     return (
         <AdminLayout>
